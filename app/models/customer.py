d924ca78d2e55842ca4038f3d2e397aa57d0fc96
guustavomc <gustavomccb@gmail.com>
2026-04-13 20:54:47 -0300

    added customer model

---
+++ app/models/customer.py
@@ -0,0 +1,55 @@
+import uuid
+
+class Customer:
+    def __init__(self, name, email, phone, customer_id: str | None):
+        self.id = customer_id if customer_id else self.generate_id()
+        self._name = name.strip().title()
+        self._email = email.strip().title()
+        self._phone = phone.strip()
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def email(self):
+        return self._email
+    
+    @property
+    def phone(self):
+        return self._phone
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @email.setter
+    def email(self, value):
+        self._email = value
+
+    @phone.setter
+    def phone(self, value):
+        self._phone = value
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "name": self._name,
+            "email": self._email,
+            "phone": self._phone
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            name = data["name"], 
+            email = data["email"], 
+            phone = data["phone"], 
+            customer_id = data["id"]
+        )
+    
+    def __str__(self):
+        return (
+            f"[{self.id}] {self._name} | "
+            f"Email: {self._email} | Telefone: {self._phone}"
+        )
