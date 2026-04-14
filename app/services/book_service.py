c3a909522d3ae5e77ab4127ca4b34ae82ea7552c
guustavomc <gustavomccb@gmail.com>
2026-04-13 22:09:33 -0300

    added register book method in service class

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -2,7 +2,7 @@ import json
 import csv
 import os
 
-from app.models import book
+from models.book import Book
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -17,3 +17,22 @@ def load_books():
 def save_books(books):
     with open(BOOKS_FILE, 'w', encoding="utf-8") as f:
         json.dump([book.to_dict() for book in books], f, ensure_ascii=False, indent=2)
+
+def register_book(books):
+    print("\n---Register Book---")
+    title = input("Title: ").strip()
+    author = input("Author: ").strip()
+    pages = int(input("Pages: ").strip())
+    price = float(input("Price: ").strip())
+    edition = int(input("Edition: ").strip())
+
+
+    book = Book(title, author, pages)
+    book.price = price
+    book.edition = edition
+
+    books.append(book)
+    save_books(books)
+    print("Book Registered")
+    print(book)
+    
