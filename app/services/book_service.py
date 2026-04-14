609d75676a5e711e84103b12e8d2c5b410084446
guustavomc <gustavomccb@gmail.com>
2026-04-13 21:47:22 -0300

    added save_books in service books

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -2,7 +2,8 @@ import json
 import csv
 import os
 
-from models.book import Book
+from app.models import book
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 BOOKS_FILE = os.path.join(BASE_DIR, "data", "books.json")
@@ -11,4 +12,8 @@ BOOKS_CSV = os.path.join(BASE_DIR, "data", "books.csv")
 def load_books():
     with open(BOOKS_FILE, 'r', encoding="utf-8") as f:
         data = json.load(f)
-        return [Book.from_dict(item) for item in data] 
+        return [book.from_dict(item) for item in data] 
+    
+def save_books(books):
+    with open(BOOKS_FILE, 'w', encoding="utf-8") as f:
+        json.dump([book.to_dict() for book in books], f, ensure_ascii=False, indent=2)
